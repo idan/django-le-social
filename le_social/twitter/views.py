c630c40e41a1ccb3638eb3c90285ce2a2998a397
Idan Gazit <idan@gazit.me>
2012-05-16 14:56:05 +0300

    Complete Twitter CBV subclasses

--- le_social/twitter/views.py
+++ le_social/twitter/views.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from ..oauth.views import OAuth1Authenticate
+from ..oauth.views import OAuth1Authenticate, OAuth1Callback
 
 
 class TwitterAuthenticate(OAuth1Authenticate):
@@ -8,3 +8,11 @@ class TwitterAuthenticate(OAuth1Authenticate):
     """
     def get_request_token_endpoint(self):
         return 'https://api.twitter.com/oauth/request_token'
+
+    def get_authenticate_endpoint(self):
+        return 'https://api.twitter.com/oauth/authenticate'
+
+
+class TwitterCallback(OAuth1Callback):
+    def get_access_token_endpoint(self):
+        return 'https://api.twitter.com/oauth/access_token'
